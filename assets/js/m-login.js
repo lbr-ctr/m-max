3d193ae78e0d8c3493eb6a737c3bd875ece2aae8
lbr <1602600617@qq.com>
2020-09-13 14:15:20 +0800

    主页提交1

--- assets/js/m-login.js
+++ assets/js/m-login.js
@@ -1,7 +1,4 @@
 $(function(){
-
-
-
     // 注册按钮
    $("#link_reg").on("click",function(){
 
@@ -19,7 +16,9 @@ $(function(){
         console.log(222);
     })
 
-    自定义表达验证
+    var form = layui.form
+    var layer = layui.layer
+    // 自定义表达验证
     form.verify({
 
         //我们既支持上述函数式的方式，也支持下述数组的形式
@@ -27,8 +26,73 @@ $(function(){
         pass: [
           /[0-9\.\-]{6,12}$/
           ,'密码必须6到12位，且不能出现空格'
-        ] 
+        ] ,
+
+
+              // 通过形参拿到的是确认密码框中的内容
+                // 还需要拿到密码框中的内容
+            // 然后进行一次等于的判断
+        // 如果判断失败,则return一个提示消息即可;
+
+
+        psw:function(vlue){
+
+            // 用属性选择器 选择密码框的
+            var pwd = $('.reg-box [name=password]').val();
+
+            if(vlue!==pwd) return "两次密码不一致";
+
+
+        }
       });  
 
 
+      $('#form_reg').on('submit', function(e) {
+        e.preventDefault()
+        var data = {
+            username: $('#form_reg [name = username]').val(),
+            password: $('#form_reg [name = password]').val()
+        }
+
+        $.ajax({
+            url: "/api/reguser",
+            type: "POST",
+            data: data,
+            success: function(res) {
+                console.log(data);
+                if (res.status != 0) {
+                    return alert("注册失败")
+                } else {
+                    var layer = layui.layer;
+                    layer.msg('成功');
+                    $("#link_login").click();
+                }
+            }
+        })
+    })
+
+    $("#form_login").on("submit",function(e){
+
+        e.preventDefault();
+
+        $.ajax({
+            url: "/api/login",
+            type: "POST",
+            data: $(this).serialize(),
+            success: function(res) {
+                console.log(res);
+                if (res.status != 0) {
+                    return layer.msg('失败');
+                }
+                layer.msg("成功111");
+                console.log(res.token);
+                localStorage.setItem("token",res.token)
+                location.href = '/m-index.html';
+            }
+        })    
+
+
+    })
+
 })
+
